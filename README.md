25c270158175cbb0a9e3badbfcfea779c0dcaf1e
Your Name <you@example.com>
2022-03-12 06:38:14 +0000

    repo added

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Jenkins_E2E_Automation
+# Jenkins-Automation-Python
+
+| Deployment | Type| Author |
+| -------- | -------- |--------|
+| Jenkins python Automation |Cloud  | BALAMURUGAN BASKARAN|
+
+# Purpose
+The purpose of this project is listed below:
+  1) Create Jenkins file at any Github repo from jinja2 template
+  2) Create pipeline in jenkins with newly created jenkins file
+  3) Data Visualization of CI/CD.
+
+In this project , I automated above 3 steps in single flow using Jenkins
+
+# Jenkins Job with Build Parameters
+Create Jenkins pipeline Job and Pass parameters to the job which is supplied as arguments to python script.
+
+![Jenkins Pipeline Job](https://user-images.githubusercontent.com/47313756/155072524-86b0fcdb-10ce-4dda-979f-38cec4959e44.png)
+
+```
+pipeline{
+    agent any
+    environment {
+        PAT = credentials('pat')
+        jenkins_password = credentials('jenkins_password')
+        jenkins_user = credentials('jenkins_username')
+    }
+    stages{
+        stage('Checkout'){
+            steps{
+                checkout([$class: 'GitSCM',
+                branches: [[name: '**']],
+                extensions: [],
+                userRemoteConfigs: [[
+                    credentialsId: 'c94b22eb-6c7d-440b-b468-06679d537899',
+                    url: 'https://github.com/bala280597/Jenkins-Automation-Python.git']]])
+            }
+        }
+        stage(" Jenkins file creation "){
+          steps {
+              script {
+                properties([parameters([
+            	string(defaultValue: 'bala280597/Nodejs', description: 'Repository Name in Github', name: 'REPO'), 
+            	string(defaultValue: 'heads/sample', description: 'Github Branch', name: 'BRANCH'), 
+            	choice(choices: ['Docker', 'Maven'], description: 'Type of Build', name: 'BUILD_TYPE'), 
+            	choice(choices: ['Kubernetes'], description: 'Type of Deployment', name: 'DEPLOY_TYPE'), 
+            	string(defaultValue: 'Sample-test', description: 'Name of Jenkins Job', name: 'JENKINS_PIPELINE_JOB_NAME')
+            	])])
+              }
+                    sh """ 
+                           python job.py $PAT ${params.REPO} ${params.BRANCH} ${params.BUILD_TYPE} ${params.DEPLOY_TYPE} ${params.JENKINS_PIPELINE_JOB_NAME} $jenkins_user     $jenkins_password
+                  """  
+          }
+        }
+    }
+}
+
+```
+# Sonar Integration
+Sonarqube is integrated to analyze source code.
+![image](https://user-images.githubusercontent.com/47313756/155874508-288d7e40-9f6f-41a6-ad0c-ccd22d307f88.png)
+
+
+# Data Visualization
+Hosted SQL Server in Kubernetes cluster as Stateful set and expose it as Load Balancer Service. Data from Jenkins job inserted into MYSQL Database.
+Data in Database is visualized in Grafana.
+
+![Grafana](https://user-images.githubusercontent.com/47313756/155077273-a9c50f98-6013-4e30-9203-4cfd64d62e73.png)
+
---
+++ job.py
@@ -0,0 +1,109 @@
+from jinja2 import Template
+import base64
+import requests
+from github import Github
+from github import InputGitTreeElement
+import sys
+import jenkins
+import mysql.connector
+
+
+class Jenkins:
+
+    def __init__(self, jobname,token,repo,branch,build,deploy,jenkins_username,jenkins_password):
+        self.jobname = jobname
+        self.token = token
+        self.repo = repo
+        self.branch = branch
+        self.build = build
+        self.deploy = deploy
+        self.jenkins_username = jenkins_username
+        self.jenkins_password = jenkins_password
+
+    def sql_insert_data(self):
+        cnx = mysql.connector.connect(user='root', password=self.jenkins_password,
+                                      host='34.70.1.185',database='metrics')
+        cursor = cnx.cursor()
+        add_app = ("INSERT INTO main "
+                   "(APP_NAME, BUILD_TYPE, DEPLOY_TYPE, JENKINS_JOB_NAME )"
+                   "VALUES (%(repo)s, %(build)s, %(deploy)s, %(jobname)s )")
+        data_app = {
+            'repo': self.repo,
+            'build': self.build,
+            'deploy': self.deploy,
+            'jobname': self.jobname
+        }
+        cursor.execute(add_app, data_app)
+        cnx.commit()
+        cnx.close()
+
+    def jenkins_pipeline(self):
+        Config_xml = open("config.xml", "r")
+        data = Config_xml.read()
+        Config_xml.close()
+        server = jenkins.Jenkins('http://34.102.134.5:8080/',username=self.jenkins_username,password=self.jenkins_password)
+        server.create_job(self.jobname, data)
+        print('Jenkins Pipeline created sucessfully with url:' + " " + "http://34.102.134.5:8080/job/" + self.jobname + "/")
+        self.sql_insert_data()
+
+    def github(self):
+        user = "bala280597"
+        password = self.token
+        g = Github(user, password)
+        repo = g.get_repo(self.repo)
+        file_list = [
+            'Jenkinsfile'
+        ]
+        file_names = [
+            'Jenkinsfile'
+        ]
+        commit_message = 'Jenkins file added '
+        master_ref = repo.get_git_ref(self.branch)
+        master_sha = master_ref.object.sha
+        print(master_sha)
+        base_tree = repo.get_git_tree(master_sha)
+        element_list = list()
+        for i, entry in enumerate(file_list):
+            with open(entry) as input_file:
+                data = input_file.read()
+            element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
+            element_list.append(element)
+        tree = repo.create_git_tree(element_list, base_tree)
+        parent = repo.get_git_commit(master_sha)
+        commit = repo.create_git_commit(commit_message, tree, [parent])
+        master_ref.edit(commit.sha)
+        self.jenkins_pipeline()
+
+    def template(self):
+        with open('Jenkinsfile.jinja2') as file_:
+            template = Template(file_.read())
+        repository=self.repo
+        repo = repository.split('/')
+        repo_owner = repo[0]
+        repo_name = repo[1]
+        render_file = template.render(BUILD_TYPE=self.build, DEPLOY_TYPE=self.deploy,REPO=repo_name)
+        f = open("Jenkinsfile", "w")
+        f.write(render_file)
+        f.close()
+        with open('config.xml.jinja2') as configfile_:
+            config_template = Template(configfile_.read())
+        config_render_file = config_template.render(REPO_NAME=repo_name, REPO_OWNER=repo_owner)
+        print(config_render_file)
+        f = open("config.xml", "w")
+        f.write(config_render_file)
+        f.close()
+        self.github()
+
+if __name__ == '__main__':
+    token = sys.argv[1]
+    repo = sys.argv[2]
+    branch = sys.argv[3]
+    build = sys.argv[4]
+    deploy = sys.argv[5]
+    jobname = sys.argv[6]
+    jenkins_username = sys.argv[7]
+    jenkins_password = sys.argv[8]
+    
+    job_automation = Jenkins(jobname,token,repo,branch,build,deploy,jenkins_username,jenkins_password)
+    job_automation.template()
+    
