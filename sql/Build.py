8ff23d6158fea980f4e868ca65a6a033380e5e97
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-03-13 18:48:28 +0530

    Update Build.py

--- sql/Build.py
+++ sql/Build.py
@@ -1,20 +1,22 @@
-def sql_insert_data(password,repo,job,build_no,time,table_name):
+import sys
+
+import mysql.connector
+def sql_insert_data(password,repo,job,build_no,time):
   cnx = mysql.connector.connect(user='root', password=password,
                                 host='34.70.1.185',database='metrics')
   cursor = cnx.cursor()
-  add_app = (f"INSERT INTO {table_name} "
+  add_app = ("INSERT INTO build"
              "(APP_NAME,JENKINS_JOB_NAME,BUILD_NUMBER,BUILD_TIME)"
              "VALUES (%(repo)s, %(job)s, %(build_no)s, %(time)s )")
   data_app = {
       'repo': repo,
-      'build': job,
-      'deploy': build_no,
-      'jobname': time
+      'job': job,
+      'build_no': build_no,
+      'time': time
   }
   cursor.execute(add_app, data_app)
   cnx.commit()
   cnx.close()
-  self.sonar_automation()
 
 if __name__ == '__main__':
     password = sys.argv[1]
@@ -22,7 +24,4 @@ if __name__ == '__main__':
     job = sys.argv[3]
     build_no = sys.argv[4]
     time = sys.argv[5]
-    table_name = sys.argv[6]
-    sql_insert_data(password,repo,job,build_no,time,table_name)
-   
-
+    sql_insert_data(password,repo,job,build_no,time)
